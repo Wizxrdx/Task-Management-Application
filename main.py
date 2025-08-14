e11febba1c60dfe2f2ff1db7ac75ad11b69c3fbe
Wizxrdx <benedick.dumol@gmail.com>
2025-08-15 04:59:07 +0800

    added start tasks and improved code readability

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ def main():
         print('[2] List all tasks')
         print('[3] update a task')
         print('[4] Mark a task as completed')
-        print('[5] Delete a task')
-        print('[6] Exit')
+        print('[5] Start a task')
+        print('[6] Delete a task')
+        print('[7] Exit')
 
         try:
-            x = int(input('Enter the number of task: '))
+            choice = int(input('Enter the number of task: '))
         except ValueError:
             print('Enter a number.')
             continue
@@ -29,21 +30,24 @@ def main():
             continue
         
         try:
-            if x == 1:
-                task_manager.create_task()
-            elif x == 2:
-                task_manager.list_tasks()
-            elif x == 3:
-                task_manager.update_task()
-            elif x == 4:
-                task_manager.complete_task()
-            elif x == 5:
-                task_manager.delete_task()
-            elif x == 6:
-                print('6')
-                break
-            else:
-                print('Select from the choices below.')
+            match choice:
+                case 1:
+                    task_manager.create_task()
+                case 2:
+                    task_manager.list_tasks()
+                case 3:
+                    task_manager.update_task()
+                case 4:
+                    task_manager.complete_task()
+                case 5:
+                    task_manager.start_task()
+                case 6:
+                    task_manager.delete_task()
+                case 7:
+                    print('Exiting...')
+                    break
+                case _:
+                    print('Select from the choices below.')
         except KeyboardInterrupt:
             print('Return to main')
 
