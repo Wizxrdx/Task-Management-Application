8216050fd2afadd22c4153eb24f1ea152c394a4f
Wizxrdx <benedick.dumol@gmail.com>
2025-08-15 04:55:43 +0800

    added helper function with input sanitization and validation using data parsers

--- src/helper.py
+++ src/helper.py
@@ -1,12 +1,23 @@
-def get_input(message, data_type=str):
+from typing import Callable, TypeVar
+from src.exceptions import TaskValidationError
+
+T = TypeVar('T')
+
+
+def get_input(message, data_parser: Callable[[str], T] = str, allow_empty: bool = False) -> T:
     data = None
     while data is None:
         try:
-            data = input(message + '\nUse CTRL + C to cancel.')
-            if data_type == int:
-                data = int(data)
-            elif data_type == float:
-                data = float(data)
+            raw = input(message + ' Use CTRL + C to cancel.\n')
+            if allow_empty and raw.strip() == '':
+                return None  # type: ignore
+            data = data_parser(raw)
+        except ValueError as e:
+            print(f'Input only type of {data_parser.__name__}')
+            continue
+        except TaskValidationError as e:
+            print(f'Input validation failed: {e}')
+            continue
         except (KeyboardInterrupt):
             raise KeyboardInterrupt('Input cancelled by user.')
     return data
--- src/task_manager.py
+++ src/task_manager.py
@@ -1,7 +1,16 @@
+from src.enums import Status
 from src.helper import get_input
 from src.task import Task
-from src.database import get_db
 from datetime import datetime
+from src.validators import (
+    parse_description,
+    parse_due_date,
+    parse_sorting,
+    parse_status,
+    parse_title,
+    parse_priority
+)
+
 
 class TaskManager:
     def __init__(self, db):
@@ -9,89 +18,83 @@ class TaskManager:
 
     def create_task(self):
         # title
-        title = get_input('Enter title: ')
-        description = get_input('Enter description: ')
-        due_date = get_input('Enter due date (YYYY-MM-DD): ')
-        priority = get_input('Enter priority (Low, Medium, High): ')
+        title = get_input('Enter title.', parse_title)
+        description = get_input('Enter description.', parse_description)
+        due_date = get_input('Enter due date (YYYY-MM-DD).', parse_due_date)
+        priority = get_input('Enter priority (Low, Medium, High).', parse_priority)
 
         task = Task(
             title=title,
             description=description,
             due_date=due_date,
-            priority=priority,
-            creation=datetime.now()
+            priority=priority
         )
+
+        print(task)
 
         idx = self.database.create_task(
             task.title,
             task.description,
             task.due_date,
             task.priority,
-            task.creation
         )
 
         print(f'Task Created with id {idx}')
 
     def list_tasks(self):
-        due_date_sorting = None
-        priority_filter = None
-        status_filter = None
-
-        while due_date_sorting is None:
-            x = self.__get_input('Enter due date sorting (ASC, DESC): ')
-            if x in ('ASC', 'DESC'):
-                due_date_sorting = x
-
-        x = self.__get_input('Enter priority (Low, Medium, High, None) or press Enter to skip: ')
-        if x in ('Low', 'Medium', 'High'):
-            priority_filter = x
-        elif x in ('None', ''):
-            priority_filter = None
-
-        x = self.__get_input('Enter status (Pending, In Progress, Completed, None) or press Enter to skip: ')
-        if x in ('Pending', 'In Progress', 'Completed'):
-            status_filter = x
-        elif x in ('None', ''):
-            status_filter = None
-
-        try:
-            page_num = int(self.__get_input('Enter page number: '))
-            limit = int(self.__get_input('Enter number of tasks per page: '))
-        except ValueError:
-            print('Invalid number.')
-            return
+        due_date_sorting = get_input('Enter due date sorting (ASC, DESC)', parse_sorting)
+        priority_filter = get_input('Enter priority (Low, Medium, High or Empty)', parse_priority, allow_empty=True)
+        status_filter = get_input('Enter status (Pending, In Progress, Completed or Empty)', parse_status, allow_empty=True)
+        page_num = get_input('Enter page number', int)
+        limit = get_input('Enter number of tasks per page', int)
 
         tasks = self.database.read_tasks(
             page_num=page_num,
             limit=limit,
             due_date=due_date_sorting,
-            priority=priority_filter,
-            status=status_filter
+            priority=priority_filter.value if priority_filter else None,
+            status=status_filter.value if status_filter else None
         )
 
         for task in tasks:
             print(task)
 
-
     def update_task(self):
-        pass
+        idx = get_input('Enter task ID to update.', int)
+        title = get_input('Enter new title (or press Enter to skip).', parse_title, allow_empty=True)
+        description = get_input('Enter new description (or press Enter to skip).', parse_description, allow_empty=True)
+        due_date = get_input('Enter new due date (or press Enter to skip).', parse_due_date, allow_empty=True)
+        priority = get_input('Enter new priority (or press Enter to skip).', parse_priority, allow_empty=True)
+
+        new_values = {}
+        if title:
+            new_values['title'] = title
+        if description:
+            new_values['description'] = description
+        if due_date:
+            new_values['due_date'] = due_date
+        if priority:
+            new_values['priority'] = priority
+
+        self.database.update_task(
+            idx,
+            **{k: v for k, v in new_values.items() if v is not None}
+        )
 
     def complete_task(self):
-        try:
-            task_id = int(self.__get_input('Enter task ID to mark as Completed: '))
-        except ValueError:
-            print('Invalid ID.')
-            return
+        task_id = get_input('Enter task ID to update.', int)
 
-        self.database.u(task_id)
+        self.database.update_task(task_id, status=Status.COMPLETED)
         print(f'Task {task_id} marked as Completed.')
 
+    def start_task(self):
+        task_id = get_input('Enter task ID to start: ', int)
+
+        self.database.update_task(task_id, status=Status.IN_PROGRESS)
+        print(f'Task {task_id} marked as In Progress.')
+
     def delete_task(self):
-        try:
-            task_id = int(self.__get_input('Enter task ID to delete: '))
-        except ValueError:
-            print('Invalid ID.')
-            return
+        task_id = get_input('Enter task ID to delete: ', int)
 
         self.database.delete_task(task_id)
         print(f'Task {task_id} deleted.')
